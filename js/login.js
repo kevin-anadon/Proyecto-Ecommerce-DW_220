91acfe0de72c2274167baa59818b86903d03203c
Kevin Anadon <mathew77200@hotmail.com>
2021-08-10 23:25:43 -0300

    Entrega 1 - login(validacion campos), productos

--- js/login.js
+++ js/login.js
@@ -1,6 +1,25 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+$(function() {
+
+    $('.btn-link[aria-expanded="true"]').closest('.accordion-item').addClass('active');
+  $('.collapse').on('show.bs.collapse', function () {
+	  $(this).closest('.accordion-item').addClass('active');
+	});
+
+  $('.collapse').on('hidden.bs.collapse', function () {
+	  $(this).closest('.accordion-item').removeClass('active');
+	});
+
+
 
 });
+
+function verificarCampos() {
+  let usuario = document.getElementById('usuario');
+  let contrasenia = document.getElementById('contrasenia');
+  // Verifico que los campos no estén vacios
+  if(usuario.value.trim() === '' || contrasenia.value.trim() === ''){
+    $('#alertaError').modal('show');
+  }else{
+    location.href = "./index.html"
+  }
+}
