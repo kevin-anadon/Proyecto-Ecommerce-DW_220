d2104d0eeb92e223d270985d54d148bf41af0ffa
Kevin Anadon <mathew77200@hotmail.com>
2021-08-13 07:37:25 -0300

    Actualizacion JAP - Entrega 1 sin terminar

--- js/login.js
+++ js/login.js
@@ -13,13 +13,39 @@ $(function() {
 
 });
 
-function verificarCampos() {
-  let usuario = document.getElementById('usuario');
-  let contrasenia = document.getElementById('contrasenia');
-  // Verifico que los campos no estén vacios
-  if(usuario.value.trim() === '' || contrasenia.value.trim() === ''){
+const iniciarSesion = () => {
+  let user = {}
+  // En caso de que esté seleccionada la casilla de Recordarme
+  // Guardo en la variable recordarSesion, localStorage de no ser así guardo sessionStorage
+  let storage =  document.getElementById('chkRecordar').checked ? window.localStorage : window.sessionStorage;
+  let usuario = document.getElementById('usuario').value;
+  let contrasenia = document.getElementById('contrasenia').value;
+  if(verificarCampos(usuario, contrasenia)){
     $('#alertaError').modal('show');
   }else{
-    location.href = "./index.html"
+    if(emailValido(usuario)){
+      $('#btnIniciar').popover('hide');
+      user.email = usuario;
+      user.contrasenia = contrasenia;
+      user.connected = true;
+      console.log("Prueba1");
+      storage.setItem('user',JSON.stringify(user));
+      return console.log("Prueba2");
+      location.href = "./index.html"
+    }else{
+      $('#btnIniciar').popover('show');
+    }
   }
+}
+
+function emailValido(email){
+  // Expresion regular que evalua que las tres partes que componen a una direccion de correo sean válidas
+  const expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  // Devuelvo true si es válido
+  return expReg.test(email);
+}
+
+function verificarCampos(usuario,contrasenia) {
+  // Devuelvo true si los campos están vacios
+  return usuario.trim() === '' || contrasenia.trim() === ''
 }
--- js/products.js
+++ js/products.js
@@ -1,6 +1,42 @@
+var productsArray = [];
+
+
+// Función que carga la lista de productos desde un array de objetos json
+function showProductsList(products){
+    let contenidoHtml = "";
+    for(product of products){
+        contenidoHtml += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" alt="` + product.desc + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ product.name +`</h4>
+                        <small class="text-muted">` + product.soldCount + ` artículos</small>
+                    </div>
+                    <div class="d-flex w-100 justify-content-between">
+                        <p class="mb-1">`+ product.description +`</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+        document.getElementById("cat-list-container").innerHTML = contenidoHtml;
+    }
+}
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+  getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            productsArray = resultObj.data;
+            //Muestro los productos
+            showProductsList(productsArray);
+        }
+    });
 });
